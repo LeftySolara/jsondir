bf1abddc88d98c10bbc88ce89f524764ae2d148c
Jalen Adams <jalen@jalenkadams.me>
2019-08-11 19:19:11 -0400

    Add function for printing some file info

--- jsondir/jsondir.py
+++ jsondir/jsondir.py
@@ -4,6 +4,9 @@ import pathlib
 from enum import Enum, unique, auto
 
 
+TAB_SIZE = 4
+
+
 @unique
 class FileType(Enum):
     """Unix file types"""
@@ -28,6 +31,18 @@ class FileType(Enum):
             if method and method(path):
                 return path_type
         return cls.UNKNOWN
+
+
+TYPE_STRINGS = {
+    FileType.DIR: "directory",
+    FileType.REG: "regular file",
+    FileType.LNK: "symbolic link",
+    FileType.SOCK: "socket",
+    FileType.FIFO: "named pipe",
+    FileType.BLK: "block device file",
+    FileType.CHR: "character special device file",
+    FileType.UNKNOWN: "unknown"
+}
 
 
 def process_args():
@@ -69,14 +84,56 @@ def process_args():
     return namespace
 
 
+def print_file(path, tab_count):
+    """Display information for a single file.
+
+    Parameters
+    ----------
+    path : pathlib.Path
+        The path to display information for.
+
+    tab_count : int
+        Number of tabs to insert for indentation.
+
+    """
+
+    output_strings = []
+
+    file_type = FileType.get_file_type(path)
+    type_str = "\"type\": \"{}\"".format(TYPE_STRINGS[file_type])
+    output_strings.append(type_str)
+
+    # For root and cwd, path.name returns an empty string.
+    # Thus, we need to manually enter the correct values.
+    abspath = str(path.resolve())
+    if abspath == "/":
+        name = "/"
+    elif abspath == str(pathlib.Path.cwd()):
+        name = "."
+    else:
+        name = path.name
+
+    name_str = "\"name\": \"{}\"".format(name)
+    output_strings.append(name_str)
+
+    if path.is_symlink():
+        target_str = "\"target\": \"{}\"".format(path.resolve())
+        output_strings.append(target_str)
+
+    for info_str in output_strings:
+        print(" " * TAB_SIZE * tab_count + info_str)
+
+
 def main():
     args = process_args()
 
-    if args.all:
-        print("--all flag passed")
-
-    print(args.files)
-
+    for filename in args.files:
+        try:
+            path = pathlib.Path(filename)
+            print()
+            print_file(path, 0)
+        except FileNotFoundError:
+            print("{}: No such file or directory.".format(filename))
 
 if __name__ == "__main__":
     main()
