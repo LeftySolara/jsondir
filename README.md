b338c20a9704b07c79a56c40e997d3c7fd4e0746
Jalen Adams <jalen@jalenkadams.me>
2019-08-14 16:23:31 -0400

    Update README and setup.py

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # jsondir
 
-A small python package that displays a directory's structure in JSON format, or creates a directory tree from a JSON file.
+A small program that displays a directory's contrnts in JSON format.
--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jsondir",
-    version="0.0.1",
+    version="0.1.0",
     author="Jalen Adams",
     author_email="jalen@jalenkadams.me",
-    description="Display a directory tree in JSON format or create one from a JSON file.",
+    description="Display a directory tree in JSON format.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/LeftySolara/jsondir",
